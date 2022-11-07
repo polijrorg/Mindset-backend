b998286429afc6d665015617b790ebe90c2319a4
IagoErrera <iagoerrera@hotmail.com>
2022-11-07 17:31:30 -0300

    add: docs

--- src/config/swagger/paths/index.ts
+++ src/config/swagger/paths/index.ts
@@ -1,7 +1,9 @@
 import userPaths from './user';
+import appointmentPaths from './appointments';
 
 const paths = {
   ...userPaths,
+  ...appointmentPaths,
 };
 
 export default paths;
--- src/config/swagger/schemas/index.ts
+++ src/config/swagger/schemas/index.ts
@@ -1,7 +1,13 @@
-import exempleSchema from './user';
+import userSchema from './user';
+import appointmentSchema from './appointment';
+import cardSchema from './card';
+import genreDataSchema from './genreData';
 
 const schema = {
-  ...exempleSchema,
+  ...userSchema,
+  ...appointmentSchema,
+  ...cardSchema,
+  ...genreDataSchema,
 };
 
 export default schema;
