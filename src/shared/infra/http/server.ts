a44ed72996b4ca92a1b68ed8f1cb64824bb002ab
IagoErrera <iagoerrera@hotmail.com>
2023-01-06 07:14:33 -0300

    fix: domain cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
