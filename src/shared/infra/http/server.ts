712b4bae578847d867a6330162c15b4675dad056
IagoErrera <iagoerrera@hotmail.com>
2023-01-06 07:21:17 -0300

    fix: allow all origins

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -28,9 +28,9 @@ app.use(express.json());
 app.use(routes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
