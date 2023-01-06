70f262d31a2ed2208662011a54bc0eab1bc1d11a
IagoErrera <iagoerrera@hotmail.com>
2023-01-06 07:09:21 -0300

    add: methods to cors header

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -29,6 +29,7 @@ app.use(routes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
