6abf142960b573d3859648130d02e5685e4fe51b
IagoErrera <iagoerrera@hotmail.com>
2023-01-06 06:56:55 -0300

    add: cors headers

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -27,6 +27,12 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
